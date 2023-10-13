705083635d63d924ea337ca79aeda50027aa201b
Georgios11 <tsompanidis.georgios@gmail.com>
2023-10-13 23:53:21 +0200

    functions started

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -3,45 +3,58 @@
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, menuIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[menuIndex]];
-  },
-  orderDelivery: function (time, address, mainIndex, starterIndex) {
-    console.log(
-      `Order placed! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
+const book = function (flightnum, name) {
+  console.log(`${name} has booked a seat on ${this.name}
+    flight ${this.iatacode} ${flightnum}`);
+  this.bookings.push([{ flight: `${this.iatacode}${flightnum}`, name }]);
 };
+const lufthansa = {
+  name: 'Lufthansa',
+  iatacode: 'LH',
+  bookings: [],
+};
+book.call(lufthansa, 345, 'Georgios Tsompanidis');
+const flightData = [434, 'Rania Siona'];
+book.apply(lufthansa, flightData);
+console.log(lufthansa);
+// Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+//   order: function (starterIndex, menuIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[menuIndex]];
+//   },
+//   orderDelivery: function (time, address, mainIndex, starterIndex) {
+//     console.log(
+//       `Order placed! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+//     );
+//   },
+// };
 // restaurant.orderDelivery({
 //   time: '23:30',
 //   address: 'Cornelis',
 //   mainIndex: 2,
 //   starterIndex: 2,
 // });
-console.log(restaurant.order(2, 2));
+// console.log(restaurant.order(2, 2));
 ////OBJECT DESTRUCTURING////
 
 // const { name, openingHours, categories } = restaurant;
@@ -104,3 +117,71 @@ console.log(restaurant.order(2, 2));
 // // console.log(i, j);
 // const [i, , [j, k]] = nested;
 // console.log(i, j, k);
+
+//The for of loop
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const item of menu) console.log(item);
+
+// //to show the index
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+// console.log(menu.entries());
+// console.log(...menu.entries());
+// console.log([...menu.entries()]);
+
+//ENHANCED OBJECT LITERALS////
+// const openingHours = {
+//   thu: {
+//     open: 12,
+//     close: 22,
+//   },
+//   fri: {
+//     open: 11,
+//     close: 23,
+//   },
+//   sat: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//   order: function (starterIndex, menuIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[menuIndex]];
+//   },
+//   orderDelivery: function (time, address, mainIndex, starterIndex) {
+//     console.log(
+//       `Order placed! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+//     );
+//   },
+//   openingHours,
+// };
+// console.log(restaurant);
+// const entries = Object.entries(restaurant);
+// console.log(entries);
+// const [entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8] =
+//   entries;
+// const [, , , , , , , [hours, { thu: pem, fri: par, sat: sav, sun: kyr }]] =
+//   entries;
+// console.log(hours, pem, par, sav);
+
+// console.log(entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8);
+// const allEntries = [
+//   ...entry1,
+//   ...entry2,
+//   ...entry3,
+//   ...entry4,
+//   ...entry5,
+//   ...entry6,
+//   ...entry7,
+//   ...entry8,
+// ];
+// console.log(allEntries);
+
+// console.log(entry8);
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,85 @@
 'use strict';
+
+/////DEFAULT PARAMETERS
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES5 Default Parameters
+  //   numPassengers ||= 1;
+  //   price ||= 1;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+// createBooking('LH123');
+// createBooking('LH123', 2, 688);
+
+//we can skip a parameter by passing undefined
+// createBooking('LH123', undefined, 555);
+
+////HOW PASSING ARGUMENTS WORKS: VALUE VS. REFERENCE
+
+const flight = 'LH234';
+
+const geo = {
+  name: 'Georgios Tsompanidis',
+  passport: 12345678,
+};
+
+const checkIn = function (flight, passenger) {
+  //When primitive values are passed into functions, a copy of the value is created in the function, therefore primitive values  can NOT be changed by functions
+  flight = 'LH567';
+
+  //Reference values are passed into functions, WE DO NOT PASS BY REFERENCE,  we PASS A REFERENCE reference in memory, therefore reference values CAN be changed by functions
+  passenger.name = 'Tsitsikaki';
+  if (passenger.passport === 12345678) {
+    console.log('Check In');
+  } else {
+    console.log('Wrong passport');
+  }
+};
+// checkIn(flight, geo);
+// console.log(flight);
+// console.log(geo);
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+// newPassport(geo);
+// console.log(geo);
+// checkIn(flight, geo);
+
+//FIRST CLASS FUNCTIONS AND HIGHER-ORDER FUNCTIONS
+
+//First class functions is a FEATURE-CONCEPT that a programming language either has or does not have
+
+//It means that all functions ARE VALUES
+
+//Higher Order functions exist in practice and are possible because JS has first class functions
+
+//HIGHER ORDER FUNCTION is a function that RECEIVES another function as an argument , RETURNS a function or BOTH
+
+////FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-Order function
+const transformer = function (str, fn) {
+  console.log(`Transformed string ${fn(str)}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
